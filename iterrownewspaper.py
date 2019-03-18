515661215353aef128956adbeb29cae05daf25d1
afran14 <44987359+afran14@users.noreply.github.com>
2019-03-18 18:48:33 -0400

    Add files via upload

--- iterrownewspaper.py
+++ iterrownewspaper.py
@@ -5,18 +5,23 @@ import uuid
 
 data = pd.read_csv('./candidate_coverage.csv', encoding='utf8')
 
-for index,row in data.iterrows():
+df_done = data[data['text'].str.len() > 0]
+df_not_done = data[data['text'].isnull()]
+
+for index,row in df_not_done.iterrows():
     x = row['link']
     article = Article(x, language='en')
     article.download()
     article.parse()
     file_name = str(row['candidate']) + "-" + str(row['date'] + "-" + str(row['outlet'])) + "-" + str(uuid.uuid4())[:5]
-    data.loc[index,'file'] = file_name
-    data.loc[index, 'text'] = article.text
+    df_not_done.loc[index,'file'] = file_name
+    df_not_done.loc[index, 'text'] = article.text
     with open(file_name, 'w') as f:
         f.write(article.text)
 
-data.to_csv('./candidate_coverage.csv', encoding='utf8', index=False)
+complete_data = pd.concat([df_done,df_not_done])
+
+complete_data.to_csv('./candidate_coverage.csv', encoding='utf8', index=False)
 
 
 
